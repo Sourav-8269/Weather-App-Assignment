880a5f75b157dfe5b24f1a7fda5add781e029542
Sourav <souravsahu70047@gmail.com>
2023-06-22 22:02:45 +0530

    Added Files

--- Frontend/weather-app/src/App.js
+++ Frontend/weather-app/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import axios from "axios"
+import Navbar from './components/Navbar';
 
 function App() {
+  useEffect(() => {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=40.7127281&lon=-74.0060152&units=metric&appid=19e5bb78eb85f21fbfcbcde9f026af1e`)
+    // axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=london&limit=5&units=metric&appid=19e5bb78eb85f21fbfcbcde9f026af1e`)
+    .then((res)=>console.log(res))
+    .catch((err)=>console.log(err))
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header"> */}
+        <Navbar/>
+      {/* </header> */}
     </div>
   );
 }
---
+++ Frontend/weather-app/src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "../styles/navbar.css";
+import {
+  Box,
+  Button,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Text,
+} from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
+import { BiCurrentLocation } from "react-icons/bi";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div id="logo">
+        <Text fontFamily="cursive" fontSize={["20px","24px", "30px"]}>
+        SkyCast
+        </Text>
+      </div>
+      <div id="search">
+        <InputGroup>
+          <InputLeftElement pointerEvents="none">
+            <Search2Icon color="gray.300" />
+          </InputLeftElement>
+          <Input
+            // htmlSize={40}
+            type="tel"
+            placeholder="Enter your location"
+            id="input"
+            bg="#3f49c026"
+            boxShadow="rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
+          />
+        </InputGroup>
+      </div>
+      <div id="location">
+        <Button
+          display={{ base: "none", md: "flex" }}
+          borderBox="rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
+          borderRadius="30px"
+          leftIcon={<BiCurrentLocation size={25} />}
+          colorScheme="messenger"
+        >
+          Current Location
+        </Button>
+        <IconButton
+        display={{ md: 'none' }}
+          bg="#0078ff"
+          borderRadius="50%"
+          textAlign="center"
+          isRound={true}
+          // display="flex"
+          alignItems="center"
+          justifyContent="center"
+          colorScheme='blue'
+          p={2}
+        >
+          <BiCurrentLocation size={25} alignmentBaseline="center" />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
